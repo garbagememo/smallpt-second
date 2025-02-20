41a4092f7ee21292350c99713838486fec351f0c
garbagememo <66831242+garbagememo@users.noreply.github.com>
2025-02-20 15:31:30 +0900

    smallptのスレッド対応とBVHの一部適用

--- spne.pas
+++ spne.pas
@@ -3,7 +3,7 @@
 {$INLINE ON}
 {$modeswitch advancedrecords}
 
-uses SysUtils,Classes,uVect,uDetector,uBMP,Math,getopts;
+uses SysUtils,Classes,uVect,uDetector,uScene,uBMP,Math,getopts;
 
 type 
   CamRecord=record
@@ -39,165 +39,6 @@ begin
    dirct:=dirct.norm;
    result.o:= dirct* 140+o;
    result.d := dirct;
-end;
-
-function radiance(const r:RayRecord;depth:integer):Vec3;
-var
-  id:integer;
-  obj:SphereClass;
-  x,n,nl,f:Vec3;
-  uvw:Vec3Matrix;
-  p,t:real;
-  into:boolean;
-  ray2,RefRay:RayRecord;
-  nc,nt,nnt,ddn,cos2t,q,a,b,c,R0,Re,RP,Tr,TP:real;
-  tDir:Vec3;
-begin
-  id:=0;depth:=depth+1;
-  if intersect(r,t,id)=false then begin
-    result:=ZeroVec;exit;
-  end;
-  obj:=SphereClass(sph[id]);
-  x:=r.o+r.d*t; n:=(x-obj.p).norm; f:=obj.c;
-  if n.dot(r.d)<0 then nl:=n else nl:=n*-1;
-  if (f.x>f.y)and(f.x>f.z) then
-    p:=f.x
-  else if f.y>f.z then 
-    p:=f.y
-  else
-    p:=f.z;
-   if (depth>5) then begin
-    if random<p then 
-      f:=f/p 
-    else begin
-      result:=obj.e;
-      exit;
-    end;
-  end;
-  case obj.refl of
-    DIFF:begin
-      result:=obj.e+f.Mult(radiance(ray2.new(x,uvw.GetUniformVec(nl) ),depth) );
-      //GetUniformVecは引数vecを中心に半円状に一様分布するVecを算出する
-    end;(*DIFF*)
-    SPEC:begin
-      result:=obj.e+f.mult(radiance(ray2.new(x,r.d-n*2*(n*r.d) ),depth));
-    end;(*SPEC*)
-    REFR:begin
-      RefRay.new(x,r.d-n*2*(n*r.d) );
-      into:= (n*nl>0);
-      nc:=1;nt:=1.5; if into then nnt:=nc/nt else nnt:=nt/nc; ddn:=r.d*nl; 
-      cos2t:=1-nnt*nnt*(1-ddn*ddn);
-      if cos2t<0 then begin   // Total internal reflection
-        result:=obj.e + f.mult(radiance(RefRay,depth));
-        exit;
-      end;
-      if into then q:=1 else q:=-1;
-      tdir := (r.d*nnt - n*(q*(ddn*nnt+sqrt(cos2t)))).norm;
-      if into then Q:=-ddn else Q:=tdir*n;
-      a:=nt-nc; b:=nt+nc; R0:=a*a/(b*b); c := 1-Q;
-      Re:=R0+(1-R0)*c*c*c*c*c;Tr:=1-Re;P:=0.25+0.5*Re;RP:=Re/P;TP:=Tr/(1-P);
-      if depth>2 then begin
-        if random<p then // 反射
-          result:=obj.e+f.mult(radiance(RefRay,depth)*RP)
-        else //屈折
-          result:=obj.e+f.mult(radiance(ray2.new(x,tdir),depth)*TP);
-      end
-      else begin// 屈折と反射の両方を追跡
-        result:=obj.e+f.mult(radiance(RefRay,depth)*Re+radiance(ray2.new(x,tdir),depth)*Tr);
-      end;
-    end;(*REFR*)
-  end;(*CASE*)
-end;
-
-
-function radiance_ne_rev(r:RayRecord;depth:integer;E:integer):Vec3;
-var
-  id,i,tid:integer;
-  obj,s:DetectorClass;
-  x,n,f,nl,d:Vec3;
-  p,t:real;
-  into:boolean;
-  Ray2,RefRay:RayRecord;
-  nc,nt,nnt,ddn,cos2t,q,a,b,c,R0,Re,RP,Tr,TP:real;
-  tDir:Vec3;
-  EL,l:Vec3;
-
-  uvw:Vec3Matrix;
-begin
-  id:=0;depth:=depth+1;
-  if intersect(r,t,id)=false then begin
-    result:=ZeroVec;exit;
-  end;
-  obj:=DetectorClass(sph[id]);
-  x:=r.o+r.d*t;  f:=obj.c;
-  n:=obj.GetNormVec(x);
-  if n.dot(r.d)<0 then nl:=n else nl:=n*-1;
-  if (f.x>f.y)and(f.x>f.z) then
-    p:=f.x
-  else if f.y>f.z then 
-    p:=f.y
-  else
-    p:=f.z;
-   if (depth>5) then begin
-    if random<p then 
-      f:=f/p 
-    else begin
-      result:=obj.e;
-      exit;
-    end;
-  end;
-  case obj.refl of
-    DIFF:begin
-      d:=uvw.GetUniformVec(nl);
-        // Loop over any lights
-      EL:=ZeroVec;
-      tid:=id;
-      for i:=0 to sph.count-1 do begin
-        s:=DetectorClass(sph[i]);
-        if (i=tid) then begin
-          continue;
-        end;
-        if (s.e.x<=0) and  (s.e.y<=0) and (s.e.z<=0)  then continue; // skip non-lights
-        l:=s.GetLightVec(x,obj);
-        
-        if intersect(Ray2.new(x,l),t,id) then begin
-          if id=i then begin
-            tr:=l*nl;
-            if tr<0 then tr:=0;
-            EL:=EL+f.mult(s.e)*tr*s.Omega;
-          end;
-        end;
-      end;(*for*)
-      result:=obj.e*E+EL+f.Mult(radiance_ne_rev(ray2.new(x,d),depth,0) );
-    end;(*DIFF*)
-    SPEC:begin
-      result:=obj.e+f.mult(radiance_ne_rev(ray2.new(x,r.d-n*2*(n*r.d) ),depth,1));
-    end;(*SPEC*)
-    REFR:begin
-      RefRay.new(x,r.d-n*2*(n*r.d) );
-      into:= (n*nl>0);
-      nc:=1;nt:=1.5; if into then nnt:=nc/nt else nnt:=nt/nc; ddn:=r.d*nl; 
-      cos2t:=1-nnt*nnt*(1-ddn*ddn);
-      if cos2t<0 then begin   // Total internal reflection
-        result:=obj.e + f.mult(radiance_ne_rev(RefRay,depth,1));
-        exit;
-      end;
-      if into then q:=1 else q:=-1;
-      tdir := (r.d*nnt - n*(q*(ddn*nnt+sqrt(cos2t)))).norm;
-      if into then Q:=-ddn else Q:=tdir*n;
-      a:=nt-nc; b:=nt+nc; R0:=a*a/(b*b); c := 1-Q;
-      Re:=R0+(1-R0)*c*c*c*c*c;Tr:=1-Re;P:=0.25+0.5*Re;RP:=Re/P;TP:=Tr/(1-P);
-      if depth>2 then begin
-        if random<p then // 反射
-          result:=obj.e+f.mult(radiance_ne_rev(RefRay,depth,1)*RP)
-        else //屈折
-          result:=obj.e+f.mult(radiance_ne_rev(ray2.new(x,tdir),depth,1)*TP);
-      end
-      else begin// 屈折と反射の両方を追跡
-        result:=obj.e+f.mult(radiance_ne_rev(RefRay,depth,1)*Re+radiance_ne_rev(ray2.new(x,tdir),depth,1)*Tr);
-      end;
-    end;(*REFR*)
-  end;(*CASE*)
 end;
 
 var
@@ -296,5 +137,12 @@ begin
   end;(*for y*)
   writeln('The time is : ',TimeToStr(Time));
   writeln('elapsed time : ',TimeToStr(Time-StartDateTime) );
-  BMP.WriteBMPFile(FN);
+
+  if UpperCase(ExtractFileExt(FN))='.BMP' then
+    BMP.WriteBMPFile(FN)
+  else if UpperCase(ExtractFileExt(FN))='.PNG' then
+    BMP.WritePNG(FN)
+  else
+    BMP.WritePPM(FN);
+
 end.
--- uDetector.pas
+++ uDetector.pas
@@ -13,31 +13,36 @@ const
   M_1_PI=1/PI;
 
 type
-   AABBRecord=record
-      Min,Max:Vec3;
-      function hit(r:RayRecord;tmin,tmax:real):boolean;
-      function new(m0,m1:Vec3):AABBRecord;
-      function MargeBoundBox(box1:AABBRecord):AABBRecord;
-   end;
+  SurfaceInfo=record
+    l:Vec3;
+    omega:real;
+  end;
+    
+  AABBRecord=record
+    Min,Max:Vec3;
+    function hit(r:RayRecord;tmin,tmax:real):boolean;
+    function new(m0,m1:Vec3):AABBRecord;
+    function MargeBoundBox(box1:AABBRecord):AABBRecord;
+  end;
 
   RectAxisType=(XY,YZ,XZ);(*平面がどっち向いているか*)
 
   DetectorClass=Class
     p,e,c:Vec3;// position. emission,color
     refl:RefType;
-    Omega:real;
+    Omega:real;//マルチスレッド時にはクラス内の値は使わないほうがよいのでこの形は修正すべき
     BoundBox:AABBRecord;
     constructor Create(p_,e_,c_:Vec3;refl_:RefType);virtual;
     function intersect(const r:RayRecord):real;virtual;abstract;
     function GetNormVec(x:Vec3):Vec3;virtual;abstract;
-    function GetLightVec(x:Vec3;org:DetectorClass):Vec3;virtual;abstract;
-   end;
+    function GetLightVec(x:Vec3;org:DetectorClass):SurfaceInfo;virtual;abstract;
+  end;
   SphereClass=class(DetectorClass)
     rad:real;       //radius
     constructor Create(rad_:real;p_,e_,c_:Vec3;refl_:RefType);virtual;
     function intersect(const r:RayRecord):real;override;
     function GetNormVec(x:Vec3):Vec3;override;
-    function GetLightVec(x:Vec3;org:DetectorClass):Vec3;override;
+    function GetLightVec(x:Vec3;org:DetectorClass):SurfaceInfo;override;
   end;
   RectClass=class(DetectorClass)
     H1,H2,V1,V2,w,h,area:Real;
@@ -46,23 +51,15 @@ type
     constructor Create(RA_:RectAxisType;H1_,H2_,V1_,V2_:real;p_,e_,c_:Vec3;refl_:RefType);
     function intersect(const r:RayRecord):real;override;
     function GetNormVec(x:Vec3):Vec3;override;
-    function GetLightVec(x:Vec3;org:DetectorClass):Vec3;override;
+    function GetLightVec(x:Vec3;org:DetectorClass):SurfaceInfo;override;
   end;
-
-
-var
-   sph:TList;
-
-procedure InitScene;
-procedure InitNEScene;
-procedure SkyScene;
-procedure ForestScene;
-procedure WadaScene;
-procedure RandomScene;
-procedure RectLightScene;
-procedure testScene;
   
 function intersect(const r:RayRecord;var t:real; var id:integer):boolean;
+function radiance(const r:RayRecord;depth:integer):Vec3;
+function radiance_ne_rev(r:RayRecord;depth:integer;E:integer):Vec3;
+
+var
+   sph:TList;//object list
 
 
 implementation
@@ -168,7 +165,7 @@ begin
   result:=(x-p).norm;
 end;
 
-function SphereClass.GetLightVec(x:Vec3;org:DetectorClass):Vec3;
+function SphereClass.GetLightVec(x:Vec3;org:DetectorClass):SurfaceInfo;
 var
   uvw:Vec3Matrix;
   cos_a_max,eps1,eps2,cos_a,sin_a,phi:real;
@@ -176,8 +173,8 @@ var
 begin
   if (x-p).len<rad then begin
     nl:=org.GetNormVec(x);//メインですでに実行しているので削除したいが、そうすると見通しが悪くなるので・・・
-    result:=uvw.GetUniformVec(nl);//こちらもメインで既に実行している・・・
-    Omega:=1;
+    result.l:=uvw.GetUniformVec(nl);//こちらもメインで既に実行している・・・
+    result.Omega:=1;
     exit;//result:=uvw.getNormVec;
   end
   else begin
@@ -188,18 +185,32 @@ begin
     sin_a := sqrt(1-cos_a*cos_a);
     if (1-2*random)<0 then sin_a:=-sin_a; 
     phi := M_2PI*eps2;
-    result:=(uvw.u*(cos(phi)*sin_a)+uvw.v*(sin(phi)*sin_a)+uvw.w*cos_a).norm;
-    Omega:=2*PI*(1-cos_a_max)*M_1_PI;
+    result.l:=(uvw.u*(cos(phi)*sin_a)+uvw.v*(sin(phi)*sin_a)+uvw.w*cos_a).norm;
+    result.Omega:=2*PI*(1-cos_a_max)*M_1_PI;
   end;
 end;
 
 constructor RectClass.Create(RA_:RectAxisType;H1_,H2_,V1_,V2_:real;p_,e_,c_:Vec3;refl_:RefType);
+var
+  bMin,bMax:Vec3;
 begin
   RA:=RA_;H1:=H1_;H2:=H2_;V1:=V1_;V2:=V2_;h:=H2-H1;w:=V2-V1;
   case RA of
-    XY:begin p_.x:=H1; p_.y:=V1; hv.new(H2-H1,0,0);wv.new(0,V2-V1,0);end;
-    XZ:begin p_.x:=H1; p_.z:=V1; hv.new(H2-H1,0,0);wv.new(0,0,V2-V1);end;
-    YZ:begin p_.y:=H1; p_.z:=V1; hv.new(0,H2-H1,0);wv.new(0,0,v2-v1);end;
+    XY:begin
+         p_.x:=H1; p_.y:=V1; hv.new(H2-H1,0,0);wv.new(0,V2-V1,0);
+         BoundBox.new(bMin.new(min(H1,H2),min(V1,V2),p_.z-EPS),
+                      bMax.new(Max(H1,H2),Max(V1,V2),p_.z+EPS) );
+       end;
+    XZ:begin
+         p_.x:=H1; p_.z:=V1; hv.new(H2-H1,0,0);wv.new(0,0,V2-V1);
+         BoundBox.new(bMin.new(min(H1,H2),p_.y-EPS,min(V1,V2)),
+                      bMax.new(Max(H1,H2),p_.y+EPS,Max(V1,V2)) );
+       end;
+    YZ:begin
+         p_.y:=H1; p_.z:=V1; hv.new(0,H2-H1,0);wv.new(0,0,v2-v1);
+         BoundBox.new(bMin.new(p_.x-EPS,min(H1,H2),min(V1,V2)),
+                      bMax.new(p_.x+EPS,Max(H1,H2),Max(V1,V2)) );
+       end;
   end;
   nl:=(hv/wv).norm*-1;
   area:=w*h;
@@ -248,7 +259,7 @@ begin
   result:=nl;
 end;
 
-function RectClass.GetLightVec(x:Vec3;org:DetectorClass):Vec3;
+function RectClass.GetLightVec(x:Vec3;org:DetectorClass):SurfaceInfo;
 var
   r:Vec3;
   dist,eps1,eps2:real;
@@ -261,9 +272,9 @@ begin
     XZ:begin r.x:=p.x+h*eps1;r.z:=p.z+w*eps2; r.y:=p.y end;
     YZ:begin r.y:=p.y+h*eps1;r.z:=p.z+w*eps2; r.x:=p.x end;
   end;
-  result:=(r-x).norm;
+  result.l:=(r-x).norm;
   dist:=(x-r).sqr;
-  Omega:=Area/(2*pi*dist);
+  result.Omega:=Area/(2*pi*dist);
 end;
 
 function intersect(const r:RayRecord;var t:real; var id:integer):boolean;
@@ -283,183 +294,163 @@ begin
 end;
 
 
-procedure InitScene;
+function radiance(const r:RayRecord;depth:integer):Vec3;
 var
-   p,c,e:Vec3;
+  id:integer;
+  obj:SphereClass;
+  x,n,nl,f:Vec3;
+  uvw:Vec3Matrix;
+  p,t:real;
+  into:boolean;
+  ray2,RefRay:RayRecord;
+  nc,nt,nnt,ddn,cos2t,q,a,b,c,R0,Re,RP,Tr,TP:real;
+  tDir:Vec3;
 begin
-  sph:=TList.Create;
-  sph.add( SphereClass.Create(1e5, p.new( 1e5+1,40.8,81.6),  ZeroVec,c.new(0.75,0.25,0.25),DIFF) );//Left
-  sph.add( SphereClass.Create(1e5, p.new(-1e5+99,40.8,81.6), ZeroVec,c.new(0.25,0.25,0.75),DIFF) );//Right
-  sph.add( SphereClass.Create(1e5, p.new(50,40.8, 1e5),      ZeroVec,c.new(0.75,0.75,0.75),DIFF) );//Back
-  sph.add( SphereClass.Create(1e5, p.new(50,40.8,-1e5+170),  ZeroVec,c.new(0,0,0),      DIFF) );//Front
-  sph.add( SphereClass.Create(1e5, p.new(50, 1e5, 81.6),     ZeroVec,c.new(0.75,0.75,0.75),DIFF) );//Bottomm
-  sph.add( SphereClass.Create(1e5, p.new(50,-1e5+81.6,81.6), ZeroVec,c.new(0.75,0.75,0.75),DIFF) );//Top
-  sph.add( SphereClass.Create(16.5,p.new(27,16.5,47),        ZeroVec,c.new(1,1,1)*0.999, SPEC) );//Mirror
-  sph.add( SphereClass.Create(16.5,p.new(73,16.5,88),        ZeroVec,c.new(1,1,1)*0.999, REFR) );//Glass
-  sph.add( SphereClass.Create(600, p.new(50,681.6-0.27,81.6),e.new(12,12,12),    ZeroVec,DIFF) );//Ligth
-end;
-
-procedure InitNEScene;
-var
-   p,c,e:Vec3;
-begin
-  sph:=TList.Create;
-  sph.add( SphereClass.Create(1e5, p.new( 1e5+1,40.8,81.6),  ZeroVec,c.new(0.75,0.25,0.25),DIFF) );//Left
-  sph.add( SphereClass.Create(1e5, p.new(-1e5+99,40.8,81.6), ZeroVec,c.new(0.25,0.25,0.75),DIFF) );//Right
-  sph.add( SphereClass.Create(1e5, p.new(50,40.8, 1e5),      ZeroVec,c.new(0.75,0.75,0.75),DIFF) );//Back
-  sph.add( SphereClass.Create(1e5, p.new(50,40.8,-1e5+170+eps),ZeroVec,ZeroVec            ,DIFF) );//Front
-  sph.add( SphereClass.Create(1e5, p.new(50, 1e5, 81.6),     ZeroVec,c.new(0.75,0.75,0.75),DIFF) );//Bottomm
-  sph.add( SphereClass.Create(1e5, p.new(50,-1e5+81.6,81.6), ZeroVec,c.new(0.75,0.75,0.75),DIFF) );//Top
-  sph.add( SphereClass.Create(16.5,p.new(27,16.5,47),        ZeroVec,c.new(1,1,1)*0.999,   SPEC) );//Mirror
-  sph.add( SphereClass.Create(16.5,p.new(73,16.5,88),        ZeroVec,c.new(1,1,1)*0.999,   REFR) );//Glass
-  sph.add( SphereClass.Create( 1.5,p.new(50,81.6-16.5,81.6), e.new(4,4,4)*100,   ZeroVec,  DIFF) );//Ligth
-end;
-
-procedure RandomScene;
-var
-   Cen,Cen1,Cen2,Cen3:Vec3;
-   a,b:integer;
-   RandomMatterial:real;
-   p,c,e:Vec3;
-begin
-  sph:=TList.Create;
-  Cen.new(50,40.8,-860);
-
-  Cen1.new(75,25, 85);
-  Cen2.new(45,25, 30);
-  Cen3.new(15,25,-25);
-  
-
-  sph.add(SphereClass.Create(10000,  Cen+p.new(0,0,-200)  , e.new(0.6, 0.5, 0.7)*0.8, c.new(0.7,0.9,1.0),  DIFF)); // sky
-  sph.add(SphereClass.Create(100000, p.new(50, -100000, 0), ZeroVec,                  c.new(0.4,0.4,0.4),  DIFF)); // grnd
-
-
-  sph.add(SphereClass.Create(25,  Cen1 ,ZeroVec,c.new(0.9,0.9,0.9), SPEC));// Glas
-  sph.add(SphereClass.Create(25,  Cen2 ,ZeroVec,c.new(0.95,0.95,0.95),  REFR)); // Glass
-  sph.add(SphereClass.Create(25,  Cen3 ,ZeroVec,c.new(1,0.6,0.6)*0.696, DIFF));    // 乱反射
-  for a:=-11 to 11 do begin
-     for b:=-11 to 11 do begin
-        RandomMatterial:=random;
-        Cen.new( (a+random)*25,5,(b+random)*25);
-        if ( (Cen - Cen1) ).len>25*1.0 then begin
-           if RandomMatterial<0.8 then begin
-              sph.add(SphereClass.Create(5,Cen,ZeroVec,c.new(random,Random,random),DIFF));
-           end
-           else if RandomMatterial <0.95 then begin
-              sph.add(SphereClass.Create(5,Cen,ZeroVec,c.new(random,Random,random),SPEC));
-           end
-           else begin
-              sph.add(SphereClass.Create(5,Cen,ZeroVec,c.new(random,Random,random),REFR));
-           end;
-        end;
-     end;
+  id:=0;depth:=depth+1;
+  if intersect(r,t,id)=false then begin
+    result:=ZeroVec;exit;
   end;
+  obj:=SphereClass(sph[id]);
+  x:=r.o+r.d*t; n:=(x-obj.p).norm; f:=obj.c;
+  if n.dot(r.d)<0 then nl:=n else nl:=n*-1;
+  if (f.x>f.y)and(f.x>f.z) then
+    p:=f.x
+  else if f.y>f.z then 
+    p:=f.y
+  else
+    p:=f.z;
+   if (depth>5) then begin
+    if random<p then 
+      f:=f/p 
+    else begin
+      result:=obj.e;
+      exit;
+    end;
+  end;
+  case obj.refl of
+    DIFF:begin
+      result:=obj.e+f.Mult(radiance(ray2.new(x,uvw.GetUniformVec(nl) ),depth) );
+      //GetUniformVecは引数vecを中心に半円状に一様分布するVecを算出する
+    end;(*DIFF*)
+    SPEC:begin
+      result:=obj.e+f.mult(radiance(ray2.new(x,r.d-n*2*(n*r.d) ),depth));
+    end;(*SPEC*)
+    REFR:begin
+      RefRay.new(x,r.d-n*2*(n*r.d) );
+      into:= (n*nl>0);
+      nc:=1;nt:=1.5; if into then nnt:=nc/nt else nnt:=nt/nc; ddn:=r.d*nl; 
+      cos2t:=1-nnt*nnt*(1-ddn*ddn);
+      if cos2t<0 then begin   // Total internal reflection
+        result:=obj.e + f.mult(radiance(RefRay,depth));
+        exit;
+      end;
+      if into then q:=1 else q:=-1;
+      tdir := (r.d*nnt - n*(q*(ddn*nnt+sqrt(cos2t)))).norm;
+      if into then Q:=-ddn else Q:=tdir*n;
+      a:=nt-nc; b:=nt+nc; R0:=a*a/(b*b); c := 1-Q;
+      Re:=R0+(1-R0)*c*c*c*c*c;Tr:=1-Re;P:=0.25+0.5*Re;RP:=Re/P;TP:=Tr/(1-P);
+      if depth>2 then begin
+        if random<p then // 反射
+          result:=obj.e+f.mult(radiance(RefRay,depth)*RP)
+        else //屈折
+          result:=obj.e+f.mult(radiance(ray2.new(x,tdir),depth)*TP);
+      end
+      else begin// 屈折と反射の両方を追跡
+        result:=obj.e+f.mult(radiance(RefRay,depth)*Re+radiance(ray2.new(x,tdir),depth)*Tr);
+      end;
+    end;(*REFR*)
+  end;(*CASE*)
 end;
 
-procedure SkyScene;
+
+function radiance_ne_rev(r:RayRecord;depth:integer;E:integer):Vec3;
 var
-   Cen,p,e,c:Vec3;
+  id,i,tid:integer;
+  obj,s:DetectorClass;
+  x,n,f,nl,d:Vec3;
+  p,t:real;
+  into:boolean;
+  Ray2,RefRay:RayRecord;
+  nc,nt,nnt,ddn,cos2t,q,a,b,c,R0,Re,RP,Tr,TP:real;
+  tDir:Vec3;
+  EL:Vec3;
+  SufInfo:SurfaceInfo;
+  uvw:Vec3Matrix;
 begin
-  sph:=TList.Create;
-  Cen.new(50,40.8,-860);
-
-  sph.add(SphereClass.Create(1600,      p.new(1,0,2)*3000,   e.new(1,0.9,0.8)*1.2e1*1.56*2,  ZeroVec, DIFF)); // sun
-  sph.add(SphereClass.Create(1560,      p.new(1,0,2)*3500,   e.new(1,0.5,0.05)*4.8e1*1.56*2, ZeroVec,  DIFF) ); // horizon sun2
-  sph.add(SphereClass.Create(10000, Cen+p.new(0,0,-200),
-                             e.new(0.00063842, 0.02001478, 0.28923243)*6e-2*8, c.new(0.7,0.7,1)*0.25,  DIFF)); // sky
-
-  sph.add(SphereClass.Create(100000,    p.new(50, -100000, 0),ZeroVec,c.new(0.3,0.3,0.3),DIFF)); // grnd
-  sph.add(SphereClass.Create(110000,    p.new(50, -110048.5, 0),e.new(0.9,0.5,0.05)*4,ZeroVec,DIFF));// horizon brightener
-  sph.add(SphereClass.Create(4e4,       p.new(50, -4e4-30, -3000),ZeroVec,c.new(0.2,0.2,0.2),DIFF));// mountains
-
-  sph.add(SphereClass.Create(26.5,p.new(22,26.5,42),ZeroVec,c.new(1,1,1)*0.596, SPEC)); // white Mirr
-  sph.add(SphereClass.Create(13,p.new(75,13,82),ZeroVec,c.new(0.96,0.96,0.96)*0.96, REFR));// Glas
-  sph.add(SphereClass.Create(22,p.new(87,22,24),ZeroVec,c.new(0.6,0.6,0.6)*0.696, REFR));    // Glas2
+  id:=0;depth:=depth+1;
+  if intersect(r,t,id)=false then begin
+    result:=ZeroVec;exit;
+  end;
+  obj:=DetectorClass(sph[id]);
+  x:=r.o+r.d*t;  f:=obj.c;
+  n:=obj.GetNormVec(x);
+  if n.dot(r.d)<0 then nl:=n else nl:=n*-1;
+  if (f.x>f.y)and(f.x>f.z) then
+    p:=f.x
+  else if f.y>f.z then 
+    p:=f.y
+  else
+    p:=f.z;
+   if (depth>5) then begin
+    if random<p then 
+      f:=f/p 
+    else begin
+      result:=obj.e;
+      exit;
+    end;
+  end;
+  case obj.refl of
+    DIFF:begin
+      d:=uvw.GetUniformVec(nl);
+        // Loop over any lights
+      EL:=ZeroVec;
+      tid:=id;
+      for i:=0 to sph.count-1 do begin
+        s:=DetectorClass(sph[i]);
+        if (i=tid) then begin
+          continue;
+        end;
+        if (s.e.x<=0) and  (s.e.y<=0) and (s.e.z<=0)  then continue; // skip non-lights
+        SufInfo:=s.GetLightVec(x,obj);
+        if intersect(Ray2.new(x,SufInfo.l),t,id) then begin
+          if id=i then begin
+            tr:=SufInfo.l*nl;
+            if tr<0 then tr:=0;
+            EL:=EL+f.mult(s.e)*tr*SufInfo.Omega;
+          end;
+        end;
+      end;(*for*)
+      result:=obj.e*E+EL+f.Mult(radiance_ne_rev(ray2.new(x,d),depth,0) );
+    end;(*DIFF*)
+    SPEC:begin
+      result:=obj.e+f.mult(radiance_ne_rev(ray2.new(x,r.d-n*2*(n*r.d) ),depth,1));
+    end;(*SPEC*)
+    REFR:begin
+      RefRay.new(x,r.d-n*2*(n*r.d) );
+      into:= (n*nl>0);
+      nc:=1;nt:=1.5; if into then nnt:=nc/nt else nnt:=nt/nc; ddn:=r.d*nl; 
+      cos2t:=1-nnt*nnt*(1-ddn*ddn);
+      if cos2t<0 then begin   // Total internal reflection
+        result:=obj.e + f.mult(radiance_ne_rev(RefRay,depth,1));
+        exit;
+      end;
+      if into then q:=1 else q:=-1;
+      tdir := (r.d*nnt - n*(q*(ddn*nnt+sqrt(cos2t)))).norm;
+      if into then Q:=-ddn else Q:=tdir*n;
+      a:=nt-nc; b:=nt+nc; R0:=a*a/(b*b); c := 1-Q;
+      Re:=R0+(1-R0)*c*c*c*c*c;Tr:=1-Re;P:=0.25+0.5*Re;RP:=Re/P;TP:=Tr/(1-P);
+      if depth>2 then begin
+        if random<p then // 反射
+          result:=obj.e+f.mult(radiance_ne_rev(RefRay,depth,1)*RP)
+        else //屈折
+          result:=obj.e+f.mult(radiance_ne_rev(ray2.new(x,tdir),depth,1)*TP);
+      end
+      else begin// 屈折と反射の両方を追跡
+        result:=obj.e+f.mult(radiance_ne_rev(RefRay,depth,1)*Re+radiance_ne_rev(ray2.new(x,tdir),depth,1)*Tr);
+      end;
+    end;(*REFR*)
+  end;(*CASE*)
 end;
-
-procedure ForestScene;
-var
-   tc,scc,p,e,c:Vec3;
-begin
-  sph:=TList.Create;
-
-  tc:=tc.new(0.0588, 0.361, 0.0941);
-  scc:=scc.new(1,1,1)*0.7;
-  sph.add(SphereClass.Create(1e5, p.new(50, 1e5+130, 0),  e.new(1,1,1)*1.3,ZeroVec,DIFF)); //lite
-  sph.add(SphereClass.Create(1e2, p.new(50, -1e2+2, 47),  ZeroVec,c.new(1,1,1)*0.7,DIFF)); //grnd
-
-  sph.add(SphereClass.Create(1e4, p.new(50, -30, 300)+e.new(-sin(50*PI/180), 0, cos(50*PI/180))*1e4, ZeroVec, c.new(1,1,1)*0.99,SPEC));// mirr L
-  sph.add(SphereClass.Create(1e4, p.new(50, -30, 300)+e.new(sin(50*PI/180),  0, cos(50*PI/180))*1e4, ZeroVec, c.new(1,1,1)*0.99,SPEC));// mirr R
-  sph.add(SphereClass.Create(1e4, p.new(50, -30, -50)+e.new(-sin(30*PI/180), 0,-cos(30*PI/180))*1e4, ZeroVec, c.new(1,1,1)*0.99,SPEC));// mirr FL
-  sph.add(SphereClass.Create(1e4, p.new(50, -30, -50)+e.new(sin(30*PI/180),  0,-cos(30*PI/180))*1e4, ZeroVec, c.new(1,1,1)*0.99,SPEC));// mirr
-
-
-  sph.add(SphereClass.Create(4, p.new(50,6*0.6,47),                         ZeroVec, c.new(0.13,0.066,0.033), DIFF));//"tree"
-  sph.add(SphereClass.Create(16,p.new(50,6*2+16*0.6,47),                    ZeroVec, tc,  DIFF));//"tree"
-  sph.add(SphereClass.Create(11,p.new(50,6*2+16*0.6*2+11*0.6,47),           ZeroVec, tc,  DIFF));//"tree"
-  sph.add(SphereClass.Create(7, p.new(50,6*2+16*0.6*2+11*0.6*2+7*0.6,47),   ZeroVec, tc,  DIFF));//"tree"
-
-  sph.add(SphereClass.Create(15.5,p.new(50,1.8+6*2+16*0.6,47),              ZeroVec, scc,  DIFF));//"tree"
-  sph.add(SphereClass.Create(10.5,p.new(50,1.8+6*2+16*0.6*2+11*0.6,47),     ZeroVec, scc,  DIFF));//"tree"
-  sph.add(SphereClass.Create(6.5, p.new(50,1.8+6*2+16*0.6*2+11*0.6*2+7*0.6,47), ZeroVec, scc,  DIFF));//"tree"
-
-end;
-
-procedure wadaScene;
-var
-   R,T,D,Z:real;
-   p,c,e:Vec3;
-begin
-  sph:=TList.Create;
-
-  R:=60;
-  //double R=120;
-  T:=30*PI/180.;
-  D:=R/cos(T);
-  Z:=60;
-
-  sph.add(SphereClass.Create(1e5, p.new(50, 100, 0),      e.new(1,1,1)*3e0, ZeroVec           , DIFF)); // sky
-  sph.add(SphereClass.Create(1e5, p.new(50, -1e5-D-R, 0), ZeroVec,          c.new(0.1,0.1,0.1),DIFF));           //grnd
-
-  sph.add(SphereClass.Create(R, p.new(50,40.8,62)+e.new( cos(T),sin(T),0)*D, ZeroVec, c.new(1,0.3,0.3)*0.999, SPEC)); //red
-  sph.add(SphereClass.Create(R, p.new(50,40.8,62)+e.new(-cos(T),sin(T),0)*D, ZeroVec, c.new(0.3,1,0.3)*0.999, SPEC)); //grn
-  sph.add(SphereClass.Create(R, p.new(50,40.8,62)+e.new(0,-1,0)*D,           ZeroVec, c.new(0.3,0.3,1)*0.999, SPEC)); //blue
-  sph.add(SphereClass.Create(R, p.new(50,40.8,62)+e.new(0,0,-1)*D,           ZeroVec, c.new(0.53,0.53,0.53)*0.999, SPEC)); //back
-  sph.add(SphereClass.Create(R, p.new(50,40.8,62)+e.new(0,0,1)*D,            ZeroVec, c.new(1,1,1)*0.999, REFR)); //front
-end;
-
-procedure RectLightScene;
-var
-   p,c,e:Vec3;
-begin
-  sph:=TList.Create;
-  sph.add( SphereClass.Create(1e5, p.new( 1e5+1,40.8,81.6),  ZeroVec,c.new(0.75,0.25,0.25),DIFF) );//Left
-  sph.add( SphereClass.Create(1e5, p.new(-1e5+99,40.8,81.6), ZeroVec,c.new(0.25,0.25,0.75),DIFF) );//Right
-  sph.add( SphereClass.Create(1e5, p.new(50,40.8, 1e5),      ZeroVec,c.new(0.75,0.75,0.75),DIFF) );//Back
-  sph.add( SphereClass.Create(1e5, p.new(50,40.8,-1e5+170+eps),ZeroVec,ZeroVec            ,DIFF) );//Front
-  sph.add( SphereClass.Create(1e5, p.new(50, 1e5, 81.6),     ZeroVec,c.new(0.75,0.75,0.75),DIFF) );//Bottomm
-  sph.add( SphereClass.Create(1e5, p.new(50,-1e5+81.6,81.6), ZeroVec,c.new(0.75,0.75,0.75),DIFF) );//Top
-  sph.add( SphereClass.Create(16.5,p.new(27,16.5,47),        ZeroVec,c.new(1,1,1)*0.999,   SPEC) );//Mirror
-  sph.add( SphereClass.Create(16.5,p.new(73,16.5,88),        ZeroVec,c.new(1,1,1)*0.999,   REFR) );//Glass
-  sph.add( RectClass.Create(XZ,40,60,70,90,p.new(50,70,80),  e.new(4,4,4),   ZeroVec,  DIFF)  );//Ligth
-end;
-
-procedure testScene;
-var
-   p,c,e:Vec3;
-begin
-  sph:=TList.Create;
-  sph.add( SphereClass.Create(1e5, p.new( 1e5+1,40.8,81.6),  ZeroVec,c.new(0.75,0.25,0.25),DIFF) );//Left
-  sph.add( SphereClass.Create(1e5, p.new(-1e5+99,40.8,81.6), ZeroVec,c.new(0.25,0.25,0.75),DIFF) );//Right
-  sph.add( SphereClass.Create(1e5, p.new(50,40.8, 1e5),      ZeroVec,c.new(0.75,0.75,0.75),DIFF) );//Back
-  sph.add( SphereClass.Create(1e5, p.new(50,40.8,-1e5+170+eps),ZeroVec,ZeroVec            ,DIFF) );//Front
-  sph.add( SphereClass.Create(1e5, p.new(50, 1e5, 81.6),     ZeroVec,c.new(0.75,0.75,0.75),DIFF) );//Bottomm
-  sph.add( SphereClass.Create(1e5, p.new(50,-1e5+81.6,81.6), ZeroVec,c.new(0.75,0.75,0.75),DIFF) );//Top
-  sph.add( SphereClass.Create(16.5,p.new(27,16.5,47),        ZeroVec,c.new(1,1,1)*0.999,   SPEC) );//Mirror
-  sph.add( SphereClass.Create(16.5,p.new(73,16.5,88),        ZeroVec,c.new(1,1,1)*0.999,   REFR) );//Glass
-  sph.add( RectClass.Create(XZ,40,60,70,90,p.new(50,70,80),  e.new(4,4,4)*3,   ZeroVec,  DIFF)  );//Ligth
-end;
-
 
 begin
 end.
